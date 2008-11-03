7ab6080e910a25134e6f790fc2ba1bbf687981c3
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-03 16:58:00 +0000

    Added retrieval of content elements count per page Started display of overview
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/webinfo_contents/trunk@13697 735d13b6-9817-0410-8766-e36946ffe9aa

--- modfunc1/class.tx_webinfocontents_modfunc1.php
+++ modfunc1/class.tx_webinfocontents_modfunc1.php
@@ -76,6 +76,7 @@ class tx_webinfocontents_modfunc1 extends t3lib_extobjbase {
 		}
 		$theOutput .= $this->pObj->doc->spacer(5);
 		$theOutput .= $this->pObj->doc->section($GLOBALS['LANG']->getLL('title'), $sectionContent, 0, 1);
+		$theOutput .= $this->pObj->doc->spacer(10);
 
 		// Dispatch display to relevant method
 		if ($this->pObj->MOD_SETTINGS['tx_webinfocontents_modfunc1_display'] == 'search') {
@@ -93,10 +94,106 @@ class tx_webinfocontents_modfunc1 extends t3lib_extobjbase {
 	 * @return	string	HTML to display
 	 */
 	protected function displayGlobalOverview() {
+			// Get the page tree
+		$tree = $this->getPageTree();
+
+			// Parse the tree to get all the page id's
+		$uidList = array();
+		foreach ($tree->tree as $data) {
+			if (!empty($data['row']['uid'])) {
+				$uidList[] = $data['row']['uid'];
+			}
+		}
+//t3lib_div::debug($uidList);
+
+			// Get all the non-deleted content elements in those pages
+			// This query does not take into account workspaces nor language overlays
+		if (count($uidList) > 0) {
+			$where = 'pid IN ('.implode(',', $uidList).')';
+		}
+		$deleteClause = t3lib_BEfunc::deleteClause('tt_content');
+		if (!empty($deleteClause)) {
+			if (empty($where)) {
+				$where = '1 = 1';
+			}
+			$where .= $deleteClause;
+		}
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('COUNT(uid) AS total, pid, CType, list_type', 'tt_content', $where, 'pid, CType, list_type', 'pid, CType, list_type');
+			// Assemble a list of all content and plug-in types per page
+		$contentElements = array();
+		while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+			if (!isset($contentElements[$row['pid']])) $contentElements[$row['pid']] = array('standard' => array(), 'plugin' => array());
+			if ($row['CType'] == 'list') {
+				$type = 'plugin';
+				$subtype = $row['list_type'];
+			}
+			else {
+				$type = 'standard';
+				$subtype = $row['CType'];
+			}
+			$contentElements[$row['pid']][$type][] = array('subtype' => $subtype, 'count' => $row['total']);
+		}
+//t3lib_div::debug($contentElements);
+
+			// Get a list of all content element types
+			// This is taken from the TCA of tt_content, minus the dummy first element
+		$allContentElements = $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items'];
+		array_shift($allContentElements);
+
+			// Get a list of all plug-in types
+			// This is taken from the TCA of tt_content, minus the dummy first element
+		$allPlugins = $GLOBALS['TCA']['tt_content']['columns']['list_type']['config']['items'];
+		array_shift($allPlugins);
+
+			// Initialise the table layout
+		$tableLayout = array (
+			'table' => array ('<table border="0" cellspacing="1" cellpadding="2" style="width:auto;">', '</table>'),
+			'0' => array (
+				'tr' => array('<tr class="bgColor2" valign="top">', '</tr>'),
+			),
+			'defRowOdd' => array (
+				'tr' => array('<tr class="bgColor-10" valign="top">', '</tr>'),
+				'1' => array('<td class="bgColor4-20">', '</td>'),
+				'defCol' => array('<td>', '</td>'),
+			),
+			'defRowEven' => array (
+				'tr' => array('<tr class="bgColor-20" valign="top">', '</tr>'),
+				'1' => array('<td class="bgColor4-20">', '</td>'),
+				'defCol' => array('<td>', '</td>'),
+			)
+		);
+		$tableRows = array();
+		$rowCounter = 0;
+			// Define the table header row
+		$tableRows[$rowCounter] = array('Pages');
+		foreach ($allContentElements as $item) {
+			$tableRows[$rowCounter][] = $GLOBALS['LANG']->sL($item[0]);
+		}
+		foreach ($allPlugins as $item) {
+			$tableRows[$rowCounter][] = $GLOBALS['LANG']->sL($item[0]);
+		}
+		$rowCounter++;
+			// Render the page tree with the content elements for each page
+		foreach ($tree->tree as $row) {
+			$page = $row['HTML'] . t3lib_BEfunc::getRecordTitle('pages', $row['row'], TRUE);
+			$tableRows[$rowCounter] = array();
+			$tableRows[$rowCounter][] = $page;
+			$rowCounter++;
+		}
+		$table = $this->pObj->doc->table($tableRows, $tableLayout);
+		return $table;
+	}
+
+	/**
+	 * This method gets the page tree to display depening current page and chosen depth
+	 *
+	 * @return	object	Instance of t3lib_pageTree
+	 */
+	protected function getPageTree() {
 		$treeStartingPoint = intval($this->pObj->id);
 		$treeStartingRecord = t3lib_BEfunc::getRecord('pages', $treeStartingPoint);
 		t3lib_BEfunc::workspaceOL('pages', $treeStartingRecord);
-		$depth = isset($this->pObj->MOD_SETTINGS['depth']) ? $this->pObj->MOD_SETTINGS['depth'] : 2;
+		$depth = isset($this->pObj->MOD_SETTINGS['tx_webinfocontents_modfunc1_depth']) ? $this->pObj->MOD_SETTINGS['tx_webinfocontents_modfunc1_depth'] : 2;
 
 			// Initialize tree object:
 		$tree = t3lib_div::makeInstance('t3lib_pageTree');
@@ -114,13 +211,7 @@ class tx_webinfocontents_modfunc1 extends t3lib_extobjbase {
 		if ($depth > 0) {
 			$tree->getTree($treeStartingPoint, $depth, '');
 		}
-			// Parse the tree to get all the page id's
-		$uidList = array();
-		foreach ($tree->tree as $data) {
-			$uidList[] = $data['row']['uid'];
-		}
-//t3lib_div::debug($uidList);
-
+		return $tree;
 	}
 }
 
